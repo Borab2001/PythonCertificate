b86e1961581fc41f78acd024e2ca4e1467eaaaa5
Bora <borabalos9@gmail.com>
2024-04-21 14:44:56 +0200

    converting km into miles

---
+++ km_to_miles.py
@@ -0,0 +1,6 @@
+print("This program converts the kilometers into miles")
+
+km = float(input("Please enter the kilometers you want to convert: "))
+miles = float(km / 1.609344)
+
+print(km, "kilometers is equal to", round(miles,4), "miles")
