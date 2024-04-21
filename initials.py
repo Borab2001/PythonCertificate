35c190018acf847e05b73fecd191394743507622
Bora <borabalos9@gmail.com>
2024-04-21 14:46:18 +0200

    Mapping initials from input entry

---
+++ initials.py
@@ -0,0 +1,6 @@
+first_name = input("Please enter your first name: ")
+second_name = input("Please enter your second name: ")
+last_name = input("Please enter your last name: ")
+
+print("Your initials are: ", first_name[0],second_name[0],last_name[0])
+
