1bc9f835dc9cfd4bdd9a88d7fe9de8174fbe0bc9
Bora <borabalos9@gmail.com>
2024-04-24 11:16:51 +0200

    Comparing user-entered age with booleans

---
+++ boolean_age_comparison.py
@@ -0,0 +1,11 @@
+my_age = 23
+your_age = int(input("Please enter your age: "))
+
+if (my_age > your_age):
+    print("You're younger than me")
+
+elif (my_age == your_age):
+    print("We are the same age")
+
+else:
+    print("You're older than me")
