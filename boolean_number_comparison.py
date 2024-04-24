c0c569f0b28c4db185c1ad0c5e2c9a48f1c139b1
Bora <borabalos9@gmail.com>
2024-04-24 11:16:01 +0200

    Comparing numbers with boolean

---
+++ boolean_number_comparison.py
@@ -0,0 +1,11 @@
+num1 = float(input("Please enter your first number: "))
+num2 = float(input("Please enter your second number: "))
+
+if (num1 > num2):
+    print(num1, "is greater than", num2)
+
+elif (num1 == num2):
+    print(num1, "is equal to", num2)
+
+else:
+    print(num1, "is less than", num2)
