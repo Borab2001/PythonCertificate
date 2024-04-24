87d4469987834c3d999adc8c7b981e75bf341a48
Bora <borabalos9@gmail.com>
2024-04-24 11:58:57 +0200

    Attendancy grades with try/except error handling

--- attendancy_grades.py
+++ attendancy_grades.py
@@ -1,37 +1,66 @@
 data_valid = False
 
 while data_valid == False:
-    grade1 = float(input("Enter the grade of your first test: "))
+    grade1 = input("Enter the grade of your first test: ")
+    try:
+        grade1 = float(grade1)
+    except:
+        print("Invalid entry. Only numbers are accepted. Decimals should be separated with a dot.")
+        continue
+
     if grade1 < 0 or grade1 > 10:
-        print("Grade should be between 0 and 10")
+        print("Grade should be between 0 and 10.")
         continue
     else :
         data_valid = True
 
+
 data_valid = False
 
 while data_valid == False:
-    grade2 = float(input("Enter the grade of your second test: "))
+    grade2 = input("Enter the grade of your second test: ")
+    try:
+        grade2 = float(grade2)
+    except:
+        print("Invalid entry. Only numbers are accepted. Decimals should be separated with a dot.")
+        continue
+
     if grade2 < 0 or grade2 > 10:
-        print("Grade should be between 0 and 10")
+        print("Grade should be between 0 and 10.")
         continue
     else :
         data_valid = True
 
+
+        
 data_valid = False
 
 while data_valid == False:
-    number_of_classes = int(input("Enter the total number of classes: "))
+    number_of_classes = input("Enter the total number of classes: ")
+    try:
+        number_of_classes = int(number_of_classes)
+    except:
+        print("Invalid entry. Only numbers are accepted.")
+        continue
+    
     if number_of_classes <= 0:
-        print("The number of classes can't be less or equal to zero")
+        print("The number of classes can't be less or equal to zero.")
         continue
     else :
         data_valid = True
 
+
+
 data_valid = False
 
 while data_valid == False:
-    missed_classes = int(input("Enter the number of classes you missed: "))
+    missed_classes = input("Enter the number of classes you missed: ")
+    try:
+        missed_classes = int(missed_classes)
+    except:
+        print("Invalid entry. Only numbers are accepted.")
+        continue
+    
     if missed_classes < 0 or missed_classes > number_of_classes:
         print("The number of missed classes can't be less than zero or greater than the number of classes")
         continue
